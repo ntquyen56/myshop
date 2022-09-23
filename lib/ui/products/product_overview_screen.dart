ad299d685bd5e9c5f80b06ce3e38713c08d8fee9
ntquyen56 <quyenb1910287@student.ctu.edu.vn>
2022-09-23 19:51:22 +0700

    Them thong tin so san pham trong gio hang

--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/ui/cart/cart_manager.dart';
 import 'package:myshop/ui/cart/cart_screen.dart';
+import 'package:myshop/ui/products/top_right_badge.dart';
 
 import 'products_grid.dart';
 import '../shared/app_drawer.dart';
@@ -32,13 +34,16 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   }
 
   Widget buildShoppingCartIcon() {
-    return IconButton(
-      icon: const Icon(
-        Icons.shopping_cart,
+    return TopRightBadge(
+      data: CartManager().productCount,
+      child: IconButton(
+        icon: const Icon(
+          Icons.shopping_cart,
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(CartScreen.routeName);
+        },
       ),
-      onPressed: () {
-        Navigator.of(context).pushNamed(CartScreen.routeName);
-      },
     );
   }
 
