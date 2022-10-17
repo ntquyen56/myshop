22a1dded280d81f9d89e43df7a4c665bf3adf2fd
ntquyen56 <quyenb1910287@student.ctu.edu.vn>
2022-10-17 22:35:00 +0700

    Quan ly trang thai voiprovider - OrdersManager

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (ctx) => CartManager(),
         ),
+        ChangeNotifierProvider(
+          create: (ctx) => OrdersManager(),
+        ),
       ],
       child: MaterialApp(
         title: 'My Shop',
